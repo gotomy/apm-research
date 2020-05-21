ac5520547b6618625d04c4c06ff09a7542fe6369
ming.yang <ming.yang@12301.cn>
2020-05-21 14:09:16 +0800

    add skywalking oap ui

--- skywalking/docker/v7/oap_ui/docker-entrypoint.sh
+++ skywalking/docker/v7/oap_ui/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -ex
 
+[[ ! -z "$SW_OAP_ADDRESS" ]] && export COLLECTOR_RIBBON_LISTOFSERVERS=${SW_OAP_ADDRESS} && echo "COLLECTOR_RIBBON_LISTOFSERVERS=$COLLECTOR_RIBBON_LISTOFSERVERS"
+[[ ! -z "$SW_TIMEOUT" ]] && export COLLECTOR_RIBBON_READTIMEOUT=${SW_TIMEOUT} && echo "COLLECTOR_RIBBON_READTIMEOUT=$COLLECTOR_RIBBON_READTIMEOUT"
+
 exec java -jar webapp/skywalking-webapp.jar --logging.config=webapp/logback.xml "$@"
